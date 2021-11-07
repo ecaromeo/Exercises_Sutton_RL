6bfe2efdb4d79ccf7b0dfe1951f1c07a3461c38c
ecaromeo <89895671+ecaromeo@users.noreply.github.com>
2021-11-07 13:29:07 +0100

    Draft action function

--- Example 4.2 - Jack's Car Rental.py
+++ Example 4.2 - Jack's Car Rental.py
@@ -3,4 +3,17 @@ import matplotlib.pyplot as plt
 import random
 
 crnt_state = [ random.randint(0,20), random.randint(0,20)]
-np.random.poisson(lam=2)
+def action(crnt_state, move):
+    """!
+    @param crnt_state List of cars in Location 0 and 1 respectively.
+    @param move Number of cars moved from Location 0 to Location 1.
+    """
+    rqst0 = np.random.poisson(lam=3)
+    rqst1 = np.random.poisson(lam=4)
+    rtn0 = np.random.poisson(lam=3)
+    rtn1 = np.random.poisson(lam=2)
+
+    reward = (10 * np.min([crnt_state[0], rqst0])
+              + 10 * np.min([crnt_state[1], rqst1])
+              - 2 * previous_move)
+
